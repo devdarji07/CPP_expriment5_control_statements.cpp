5bb6d1d00c90bb058cc248360bf3265823460172
Dev <devkdarji@gmail.com>
2025-07-24 20:09:07 +0530

    Create Find_Even_Odd.cpp

---
+++ Find_Even_Odd.cpp
@@ -0,0 +1,24 @@
+// Dev Darji
+// 24070123033
+// Batch:A2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input_number;
+    
+    cout<<"Enter a number: ";
+    cin>>input_number;
+    
+    if(input_number%2 == 0){
+        cout<<"\n"<<"The number is Even.";
+    }
+    else if(input_number%2 != 0){
+        cout<<"\n"<<"The number is Odd.";
+    }
+    else{
+        cout<<"\n"<<"Invalid input!";
+    }
+    return 0;
+}
